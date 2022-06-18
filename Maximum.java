2a99fe14203ac931c0f439f5c951204626c3083a
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-18 11:55:57 +0530

    Refactor : All 3 UC's in one method

--- Maximum.java
+++ Maximum.java
@@ -1,6 +1,6 @@
 public class Maximum {
-    public static Integer testCase(Integer a, Integer b, Integer c) {
-        Integer max = a;
+    public static <T extends Comparable> T testCase(T a, T b, T c) {
+        T max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
@@ -10,27 +10,6 @@ public class Maximum {
         return max;
     }
 
-    public static Float testCase(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-
-    public static String testCase(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
 
     public static void main(String[] args) {
         System.out.println("Welcome Generics program to find maximum");
