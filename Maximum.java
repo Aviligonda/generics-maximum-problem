f109e7261e460fc9e6e79738e1a84312e466b478
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-18 14:28:00 +0530

    Modified : using generics method to print max

--- Maximum.java
+++ Maximum.java
@@ -1,18 +1,28 @@
 import java.util.Arrays;
 
-public class Maximum {
-    // Sorting the all numbers to find maximum there is no limit
-//    Using array method
-    public static <T> T testCase(T... a) {
-        Arrays.sort(a);
-        return a[a.length - 1];
-    }
-
+public class Maximum<T> {
     public static void main(String[] args) {
         System.out.println("Welcome Generics program to find maximum");
-        System.out.println("Maximum of Integer values is :" + testCase(2, 8, 9, 102, 6, 9, 23, 56, 95));//more than 3 variables
-        System.out.println("Maximum of Float Values is :" + testCase(2.6f, 5.6f, 2.8f, 8.9f, 10.6f));
-        System.out.println("Maximum of String values is :" + testCase("Apple", "Bird", "Juice", "Dog", "Elephant", "Fox", "Goat"));
+        Maximum<Integer> integerMaximum = new Maximum<>();
+        Maximum<Float> floatMaximum = new Maximum<>();
+        Maximum<String> stringMaximum = new Maximum<>();
+        integerMaximum.testCase(12, 8, 9, 10, 22);
+        floatMaximum.testCase(2.6f, 3.8f, 6.7f, 8.9f, 1.6f);
+        stringMaximum.testCase("Apple", "Banana", "Juice", "Sink", "Cat", "Elephant");
     }
+
+    // Sorting the all numbers to find maximum there is no limit
+//    Using array method
+    public T testCase(T... a) {
+        Arrays.sort(a);
+        T max = a[a.length - 1];
+        printMax(max);
+        return max;
+    }
+
+    private void printMax(T max) {
+        System.out.println(max);
+    }
+
 
 }
